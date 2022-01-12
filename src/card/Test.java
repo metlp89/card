8a5954615342b094a1337b0736ab5bec7d6568c1
Pavel Telpukhovski <telpukhovski@gmail.com>
2022-01-12 17:19:26 +0300

    Теперь все ок , вроде бы

--- src/card/Test.java
+++ src/card/Test.java
@@ -7,11 +7,6 @@ public class Test {
         Card creditCard = new CreditCard("Peter Parker", new BigDecimal(1500));
         Card debitCard = new DebitCard("Bruce Wayne", new BigDecimal(2000));
 
-
-        // troubles with these two methods
-        //creditCard.withdrawFromCard(new BigDecimal(1500));
-        //debitCard.withdrawFromCard(new BigDecimal(2000));
-
         ATM creditCard1 = new ATM(creditCard);
         ATM debitCard1 = new ATM(debitCard);
         System.out.println(creditCard1.withdrawFromCard(new BigDecimal(3000)));
